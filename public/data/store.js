2bb3df879d6fffb4d05596832c0f365e12dc35a8
artbeard <76746834+artbeard@users.noreply.github.com>
2022-11-04 16:28:26 +0600

    Временная загрузка данных

---
+++ public/data/store.js
@@ -0,0 +1,60 @@
+[
+    {
+      "id": 1,
+      "title": "Список покупок",
+      "userId": "ds65sf4",
+      "isActive": true,
+      "items": [
+        {
+          "id": 100,
+          "content": "Купить капусту",
+          "position": 10,
+          "complited": false
+        },
+        {
+          "id": 101,
+          "content": "Купить картошку",
+          "position": 20,
+          "complited": false
+        },
+        {
+          "id": 102,
+          "content": "Сварить борщ",
+          "position": 30,
+          "complited": false
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "title": "Дела на завтра",
+      "userId": "ds65sf4",
+      "isActive": true,
+      "items": [
+        {
+          "id": 132,
+          "content": "Постирать носки",
+          "position": 10,
+          "complited": true
+        },
+        {
+          "id": 133,
+          "content": "Погладить рубашку",
+          "position": 20,
+          "complited": false
+        },
+        {
+          "id": 134,
+          "content": "Собрать сумку",
+          "position": 30,
+          "complited": false
+        },
+        {
+          "id": 135,
+          "content": "Заказать пиццу с грибами Васе, коньяк Косте и шампанское Жене",
+          "position": 40,
+          "complited": false
+        }
+      ]
+    }
+  ]
