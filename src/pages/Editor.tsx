b408cb8e61e819be7757550b9de0d895941754a0
artbeard <76746834+artbeard@users.noreply.github.com>
2022-11-10 18:45:27 +0600

    Update Editor.tsx

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -3,42 +3,45 @@ import { useParams } from "react-router-dom";
 import { IToDoList } from '../models';
 import Store from '../store/Store';
 
+import Card from '../components/card/Card'
+import TodoListEditor from '../components/todo/TodoListEditor'
+
 function Editor() {
-    const params = useParams();
-    let TodoListId: number|null = parseInt(String(params.id));
-    let TodoList: IToDoList;
+	const params = useParams();
+	let TodoListId: number|null = parseInt(String(params.id));
+    //TodoListId = isNaN(TodoListId) ? null : TodoListId;
+    
+	let TodoList: IToDoList;
 
-    if (isNaN(TodoListId) || TodoListId === 0)
-    {
-        TodoListId = null;
-        TodoList = {
-            id: null,
-            title: 'Новый cписок',
-            userId: null,
-            isActive: true,
-            items: []
-        }
-    }
-    else
-    {
-        TodoList = Store.getTodoListById(TodoListId)
-    }
-
-     
+	if (isNaN(TodoListId) || TodoListId === 0)
+	{
+		TodoListId = null;
+		TodoList = {
+			id: null,
+			title: 'Новый cписок',
+			userId: null,
+			isActive: true,
+			items: []
+		}
+	}
+	else
+	{
+		TodoList = Store.getTodoListById(TodoListId)
+	}
 
 	return (
 		<>
-            <div className="d-flex align-items-center mb-md-3 mb-2">
+			<div className="d-flex align-items-center mb-md-3 mb-2">
 				<div className="flex-fill">
 					<ul className="breadcrumb">
 						<li className="breadcrumb-item"><a href="/">Главная</a></li>
-                        <li className="breadcrumb-item"><Link to="/todo/">Мои задачи</Link></li>
+						<li className="breadcrumb-item"><Link to="/todo/">Мои задачи</Link></li>
 						<li className="breadcrumb-item active">{TodoList.title}</li>
 					</ul>
 					<h1 className="page-header mb-0">{TodoList.title}</h1>
 				</div>
 				<div className="ms-auto">
-                    <button
+					<button
 						className="btn btn-outline-default text-nowrap btn-sm px-3 rounded-pill"
 						onClick={e=>{
 							window.history.back();
@@ -48,8 +51,14 @@ function Editor() {
 					</button>
 				</div>
 			</div>
-
-        </>
+			<div className="row">
+				<div className="col-12">
+					<Card header={`Редактирование списка `}>
+						<TodoListEditor todoListId={TodoList.id} />
+					</Card>
+				</div>
+			</div>
+		</>
 	);
 }
 
