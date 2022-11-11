62314b6b7ab849852e661a782032287a90eec6d3
artbeard <76746834+artbeard@users.noreply.github.com>
2022-11-11 18:10:17 +0600

    Create TodoListItemEditor.tsx

---
+++ src/components/todo/TodoListItemEditor.tsx
@@ -0,0 +1,34 @@
+import { observer } from 'mobx-react-lite'
+import DropDown from '../DropDown';
+import {IToDo} from '../../models'
+import Store from '../../store/Store';
+
+interface ITodoItemProps{
+	todoItem: IToDo,
+	todoListId: number | null
+}
+
+function TodoListItemEditor({todoItem, todoListId}: ITodoItemProps)
+{
+	return (
+		<>
+			<div className="form-check my-1 flex-fill">
+				<input type="checkbox" className="form-check-input"
+					id={`item_id${todoItem.id}`}
+					checked={todoItem.completed}
+					onChange={e => {Store.setCompleted(todoItem, e.target.checked)}}
+					/>
+				<label className="form-check-label"
+					htmlFor={`item_id${todoItem.id}`}>{todoItem.content}</label>
+			</div>
+			<DropDown>
+				<button className="btn btn-sm btn-link btn-outline dropdown-item">Редактировать</button>
+				<button className="btn btn-sm btn-link btn-outline dropdown-item"
+					onClick={() => {Store.removeTodo(todoItem, todoListId)}}
+					>Удалить</button>
+			</DropDown>
+		</>
+	)
+}
+
+export default observer(TodoListItemEditor);
