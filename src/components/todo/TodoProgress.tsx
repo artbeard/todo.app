136234391568f7e21e30259fba29ec919c6127d0
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-10 15:56:27 +0600

    Сокрытие прогрессбара на мобильниках

--- src/components/todo/TodoProgress.tsx
+++ src/components/todo/TodoProgress.tsx
@@ -6,10 +6,10 @@ function TodoProgress({completed}: ITodoProgress)
 {
     return (
         <>
-            <div className="progress progress-xs w-100px me-2 ms-auto" style={{height: '6px'}}>
+            <div className="progress progress-xs w-100px me-2 ms-auto d-none d-md-flex" style={{height: '6px'}}>
 				<div className="progress-bar progress-bar-striped bg-success" style={{width: (completed || 0) + '%'}}></div>
 			</div>
-			<div className="fs-12px">{completed || 0}%</div>
+			<div className="fs-12px  d-none d-md-block">{completed || 0}%</div>
 		</>
     )
 }
