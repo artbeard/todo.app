80c2acd4b1023ca953c3014ce51b86315c0fbd9c
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-13 13:29:35 +0600

    Небольшие изменения по дизайну

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -6,16 +6,20 @@ import TodoItem from './TodoItem';
 //import Store from '../../store/Store';
 import { observer } from "mobx-react-lite";
 
-interface TodoListProps{
+interface ITodoListProps{
     todoList: IToDoList
 }
-
-function TodoList({todoList}: TodoListProps)
+/**
+ * Карточка отображения списка дел на доске
+ * @param ITodoListProps
+ * @returns 
+ */
+function TodoList({todoList}: ITodoListProps)
 {
     let completedCount = todoList.items.filter(el => el.completed === true).length;
 	let totalCount = todoList.items.length;
     return (
-        <div className="list-group-item d-flex px-3">
+        <div className="list-group-item d-flex px-3 py-3">
 			<div className="me-3 pt-1">
 				<i className={'fa-fw fa-lg ' + (completedCount === totalCount 
 					? 'far fa-check-circle text-success'
@@ -23,17 +27,12 @@ function TodoList({todoList}: TodoListProps)
 					)}></i>
 			</div>
 			<div className="flex-fill">
-				<div className="fw-400 mb-2">
+				{/* <div className="fw-400 mb-2 mt-1">
 					{todoList.title}
-				</div>
+				</div> */}
 				{/* <div className="small text-white text-opacity-50 mb-2">#29930 closed yesterday by Sean</div> */}
-				<div className="mb-1">
-					{/* <Link to={'/todo/edit/' + todoList.id} className="text-right badge border border-gray-300 text-gray-300 text-decoration-none">Редактировать</Link> */}
-					{/* <span className="badge border border-gray-300 text-gray-300">Скопировать ссылку</span>
-					<span className="badge border border-success text-success">Удалить</span> */}
-				</div>
-				<hr className="my-3" />
-				<div className="d-flex align-items-center mb-2">
+				{/* <hr className="my-3" /> */}
+				<div className="d-flex align-items-center mb-2 mt-1">
 					<div className="fw-400 me-2">
 						Задачи ({totalCount}/{completedCount})
 					</div>
@@ -48,11 +47,13 @@ function TodoList({todoList}: TodoListProps)
 							: 0
 						} />
 				</div>
+                
+                <hr className="my-3" />
+
 				<div className="form-group mb-1">
 					<div className="collapse show">
 						{
 							todoList.items
-							//	.sort((a, b) => {return  a.position - b.position })
 								.map((item)=>{
 								return (
 									<TodoItem item={item} todoListID={todoList.id} key={`TodoListItem_${todoList.id}_${item.id}`} />
