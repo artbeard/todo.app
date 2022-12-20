b3787eb75eeea5b451db9e6211ebcfba2bdbca4c
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-20 16:52:56 +0600

    Update ListViewer.tsx

--- src/pages/ListViewer.tsx
+++ src/pages/ListViewer.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { observer } from "mobx-react-lite";
 import Store from '../store/Store';
 import Card from '../components/card/Card'
 import TodoListEditor from '../components/todo/TodoListEditor'
 import { 
-    //IToDoList,
-    toDoListNullObject } from '../use/models';
+	//IToDoList,
+	toDoListNullObject } from '../use/models';
 import { NotFoundError } from '../use/errors';
 /**
  * Страница Просмотр содержимого списка
@@ -15,30 +14,30 @@ import { NotFoundError } from '../use/errors';
  */
 function ListViewer() {
 	const params = useParams();
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 	//По умолчанию nullObject
-    const [todoList, setTodoList] = useState(toDoListNullObject)
-    
-    useEffect(() => {
-        //Id списка дел, получаем из роута
-        let TodoListId: number = parseInt(params.id as string);
-        try
-        {
-            setTodoList( Store.getTodoListById(TodoListId) );
-        }
-        catch (e)
-        {
-            if (e instanceof NotFoundError)
-            {
-                navigate(`/error/404`);
-            }
-            else
-            {
-                throw e;
-            }
-        }
-    }, [params.id, navigate])
-    
+	const [todoList, setTodoList] = useState(toDoListNullObject)
+	
+	useEffect(() => {
+		//Id списка дел, получаем из роута
+		let TodoListId: number = parseInt(params.id as string);
+		try
+		{
+			setTodoList( Store.getTodoListById(TodoListId) );
+		}
+		catch (e)
+		{
+			if (e instanceof NotFoundError)
+			{
+				navigate(`/error/404`);
+			}
+			else
+			{
+				throw e;
+			}
+		}
+	}, [params.id, navigate])
+	
 	return (
 		<>
 			<div className="d-flex align-items-center mb-md-3 mb-2">
@@ -63,11 +62,11 @@ function ListViewer() {
 			</div>
 			<div className="row">
 				<div className="col-12">
-                    {todoList.id !== null &&
-                        <Card header={`Редактирование списка `}>
-    						<TodoListEditor todoListId={todoList.id} />
-    					</Card>
-                    }
+					{todoList.id !== null &&
+						<Card header={`Редактирование списка `}>
+							<TodoListEditor todoListId={todoList.id} />
+						</Card>
+					}
 				</div>
 			</div>
 		</>
