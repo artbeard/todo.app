f7c5aefa7b590410965fce25b3509656b9d755e3
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-20 16:47:30 +0600

    Добавлен шаблон пользователя

---
+++ src/pages/User.tsx
@@ -0,0 +1,26 @@
+//import { useEffect, useState } from 'react'
+//import { Link, useNavigate } from 'react-router-dom';
+//import { useParams } from "react-router-dom";
+//import { observer } from "mobx-react-lite";
+//import Store from '../store/Store';
+//import Card from '../components/card/Card'
+//import TodoListEditor from '../components/todo/TodoListEditor'
+// import { 
+//     //IToDoList,
+//     toDoListNullObject } from '../use/models';
+//import { NotFoundError } from '../use/errors';
+
+/**
+ * Страница Просмотр содержимого списка
+ * @returns JSX
+ */
+function User() {
+	
+	return (
+		<>
+			Компонент пользователя
+		</>
+	);
+}
+
+export default User;
