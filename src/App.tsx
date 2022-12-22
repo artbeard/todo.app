8d83c1dfbd546b900859b41c05fd51b60dee27c2
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-22 13:38:07 +0600

    Добавлена ссылка на аккаунт у зарегистрированного пользователя

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import {useState, ReactElement} from 'react';
+import { useState, ReactElement } from 'react';
+import * as ReactDOM from 'react-dom';
 import {
 	BrowserRouter as Router,
 	Route,
 	Routes,
 	Navigate,
-	Outlet
+	Outlet,
+	Link
 } from 'react-router-dom';
 import Board from './pages/Board'
 import ListViewer from './pages/ListViewer'
 import Store from './store/Store';
 import Error from './pages/Error';
-import { getUser, getUid } from './use/getUser';
+import { getUid, getToken } from './use/getUser';
 import User from './pages/User';
-import { IUser } from './use/models'
-
 
 
 interface IProtectedRoute{
@@ -26,24 +26,16 @@ function ProtectedRoute({ userId }: IProtectedRoute): ReactElement {
 	return <Outlet /> as ReactElement;
 };
 
-
-
 function App() {
-	const [storeInit, setStoreInit] = useState(false);
 	
 	let currentUid = getUid();
-
-	getUser()
-		.then((user: IUser) => {
-			currentUid = user.id;
-			Store.init()
-				.then(()=>{
-					setStoreInit(true);
-				});
-		})
-		.catch(()=>{
+	let currentToken = getToken();
+	console.log('render app', currentUid);
+	const [storeInit, setStoreInit] = useState(false);
+	Store.init()
+		.then(()=>{
 			setStoreInit(true);
-		})
+		});
 	return (
 		<>
 		{!storeInit && 
@@ -58,9 +50,22 @@ function App() {
 		}
 		{ //storeInit &&
 			<Router basename={'/todo'}>
+				{
+					(currentUid && currentToken) &&
+					ReactDOM.createPortal(
+						(<div className="menu-item dropdown dropdown-mobile-full">
+							<Link to={`/user`} className="menu-link">
+								<div className="menu-text d-sm-block d-none">
+									<i className="far fa-user"></i>&nbsp;
+									Ваш аккаунт
+								</div>
+							</Link>
+		            	</div>),
+						document.getElementById('top-dropdown') as Element
+					)
+				}
 				<Routes>
 					<Route path="/user" element={<User/>} />
-					{/* <Route path="/user/:uid" element={<User/>} /> */}
 					<Route path="/user/:uid/:token" element={<User/>} />
 					
 					<Route element={<ProtectedRoute userId={currentUid} />}>
